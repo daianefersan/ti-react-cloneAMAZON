03d8fe4136f0a5d30d1ec2ab76d342c5d074f8b6
DAIANE FERREIRA <dra.daianeferreira@gmail.com>
2024-11-01 16:43:34 -0300

    icones adicionados/estilizei com styled-components
    adicionei fotos no assets e gerei links para usar as fotos de forma funcional dentro do projeto e fiz estilizações com styled components

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,6 +8,7 @@ const Container = styled.div`
     background-color: #0f1111;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     color: white;
 `
 const HeaderLogo = styled.div`
@@ -16,79 +17,118 @@ const HeaderLogo = styled.div`
         margin-left: 11px;
         height: 30px;
         margin-right: 20px;
+        padding: 10px 9px 10px 9px;
     }
 `
 const HeaderOptionAddress = styled.div`
-
+    padding-left: 9px;
+    display: flex;
+    align-items: center;
 `
 const OptionLineOne = styled.div`
     
 `
 const OptionLineTwo = styled.div`
-    font-weight: 700;
+    font-weight: 700; 
+    font-weight: bolder   : ;
 `
 const HeaderSearch = styled.div`
     display: flex;
+    flex-grow: 1;
+    height: 40px;
+    border-radius: 4px;
+    /* overflow: hidden; */
+    margin-left: 4px;
+    background-color: white;
+
+    :focus-within {
+        box-shadow: 0 0 0 3px #f90;
+    }
 `
 const HeaderSearchInput = styled.input`
-    
+    flex-grow: 1;
+    border: 0;
+    :focus {
+        outline: none;
+    }
 `
 const HeaderSearchIconContainer = styled.div`
-
+    background-color: #febd69;
+    width: 45px;
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 const HeaderOption = styled.div`
+    //TRouBLe
+    padding: 10px 9px 10px 9px;
     
 `
 const HeaderOptionCart = styled.div`
-
+    padding: 10px 9px 10px 9px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 const HeaderNavItems = styled.div`
     display: flex;
+    padding: 10px 9px 10px 9px;
 `
 const CartCount = styled.div`
-    
+    padding-left: 4px;
+    color: #febd69;
 `
 
 
 function Header() {
-  return (
-    <Container>
-        
-        <HeaderLogo>
-            <img src={"https://i.imgur.com/tV2N1Nw.png"}/>
-        </HeaderLogo>
+    return (
+        <Container>
 
-        <headerOptionAddress>
-            <optionalLineOne>A entrega será feita em São João ... 36680000</optionalLineOne>
-            <optionLineTwo>Atualizar CEP</optionLineTwo>
-        </headerOptionAddress>
+            <HeaderLogo>
+                <img src={"https://i.imgur.com/tV2N1Nw.png"} />
+            </HeaderLogo>
 
-        <HeaderSearch>
-            <HeaderSearchInput type='text'/>
-            <HeaderSearchIconContainer>
 
-            </HeaderSearchIconContainer>
-        </HeaderSearch>
+            <img src={"https://i.imgur.com/uh8UmMB.png"} />
+            <headerOptionAddress>
 
-        <HeaderNavItems>
 
-            <HeaderOption>
-                <optionLineOne>Olá, faça seu login </optionLineOne>
-                <optionLineTwo>Contas e Listas</optionLineTwo>
-            </HeaderOption>
+                <HeaderOption>
 
-            <HeaderOption>
-            <optionLineOne>Devoluções </optionLineOne>
-            <optionLineTwo>e Pedidos</optionLineTwo>
-            </HeaderOption>
+                    <optionalLineOne>A entrega será feita em São João ... 36680000</optionalLineOne>
+                    <optionLineTwo> Atualizar CEP</optionLineTwo>
 
-            <HeaderOptionCart>
-                <AddShoppingCartIcon />            </HeaderOptionCart>
-                <CartCount>4</CartCount>
-        </HeaderNavItems>
+                </HeaderOption>
+            </headerOptionAddress>
 
-    </Container>
-  )
+            <HeaderSearch>
+                <HeaderSearchInput type='text' />
+                <HeaderSearchIconContainer>
+                    <SearchIcon />
+                </HeaderSearchIconContainer>
+            </HeaderSearch>
+
+            <HeaderNavItems>
+
+                <HeaderOption>
+                    <optionLineOne>Olá, faça seu login </optionLineOne>
+                    <optionLineTwo>Contas e Listas</optionLineTwo>
+                </HeaderOption>
+
+                <HeaderOption>
+                    <optionLineOne>Devoluções </optionLineOne>
+                    <optionLineTwo>e Pedidos</optionLineTwo>
+                </HeaderOption>
+
+                <HeaderOptionCart>
+                    <img src={"https://i.imgur.com/dBIJhtA.png"} />
+                    <CartCount>0</CartCount>
+                </HeaderOptionCart>
+            </HeaderNavItems>
+
+        </Container>
+    )
 }
 
 export default Header
