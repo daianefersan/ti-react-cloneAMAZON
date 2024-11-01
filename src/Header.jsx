cbf9f1c0041102418220f9508c6c298509f52f5f
DAIANE FERREIRA <dra.daianeferreira@gmail.com>
2024-11-01 13:57:56 -0300

    implementando a navbar
    tentativa de acrescentar alguns icones utilizando Material Icons

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,93 @@
 import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+
+const Container = styled.div`
+    height: 60px;
+    background-color: #0f1111;
+    display: flex;
+    align-items: center;
+    color: white;
+`
+const HeaderLogo = styled.div`
+    img{
+        width: 100px;
+        margin-left: 11px;
+        height: 30px;
+        margin-right: 20px;
+    }
+`
+const HeaderOptionAddress = styled.div`
+
+`
+const OptionLineOne = styled.div`
+    
+`
+const OptionLineTwo = styled.div`
+    font-weight: 700;
+`
+const HeaderSearch = styled.div`
+    display: flex;
+`
+const HeaderSearchInput = styled.input`
+    
+`
+const HeaderSearchIconContainer = styled.div`
+
+`
+const HeaderOption = styled.div`
+    
+`
+const HeaderOptionCart = styled.div`
+
+`
+const HeaderNavItems = styled.div`
+    display: flex;
+`
+const CartCount = styled.div`
+    
+`
+
 
 function Header() {
   return (
-    <div>
+    <Container>
         
-        Header
+        <HeaderLogo>
+            <img src={"https://i.imgur.com/tV2N1Nw.png"}/>
+        </HeaderLogo>
 
-    </div>
+        <headerOptionAddress>
+            <optionalLineOne>A entrega será feita em São João ... 36680000</optionalLineOne>
+            <optionLineTwo>Atualizar CEP</optionLineTwo>
+        </headerOptionAddress>
+
+        <HeaderSearch>
+            <HeaderSearchInput type='text'/>
+            <HeaderSearchIconContainer>
+
+            </HeaderSearchIconContainer>
+        </HeaderSearch>
+
+        <HeaderNavItems>
+
+            <HeaderOption>
+                <optionLineOne>Olá, faça seu login </optionLineOne>
+                <optionLineTwo>Contas e Listas</optionLineTwo>
+            </HeaderOption>
+
+            <HeaderOption>
+            <optionLineOne>Devoluções </optionLineOne>
+            <optionLineTwo>e Pedidos</optionLineTwo>
+            </HeaderOption>
+
+            <HeaderOptionCart>
+                <AddShoppingCartIcon />            </HeaderOptionCart>
+                <CartCount>4</CartCount>
+        </HeaderNavItems>
+
+    </Container>
   )
 }
 
